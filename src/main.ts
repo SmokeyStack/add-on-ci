99c37214fac291e55eb05a39a3214be400b6a912
SmokeyStack <seansyahril04@gmail.com>
2023-02-11 12:18:56 -0500

    Revert "Debugging"
    This reverts commit a170f7f6b6e903d68649a21431e94fbef9ea2f1a.

--- src/main.ts
+++ src/main.ts
@@ -6,52 +6,48 @@ import { SummaryTableCell, SummaryTableRow } from '@actions/core/lib/summary'
 import { promises } from 'fs'
 import * as path from 'path'
 
-import { BDS_PATH } from './types/inputs'
+import {
+    BDS_PATH
+} from './types/inputs'
 import { readFile } from 'fs/promises'
 
 async function run(): Promise<void> {
-  try {
-    switch (process.platform) {
-      case 'win32': {
-        throw new Error('Unsupported platform: ${process.platform}')
-      }
-      case 'darwin': {
-        throw new Error('Unsupported platform: ${process.platform}')
-      }
-    }
-
-    await core.group('Ls', async () => {
-      await exec.exec('ls -R', [], {
-        cwd: path.join(process.cwd())
-      })
-    })
-
-    await core.group('Run bedrock server', async () => {
-      await exec.exec(
-        path.join(process.cwd(), BDS_PATH, 'bedrock_server'),
-        [],
-        {
-          cwd: path.join(process.cwd())
+    try {
+        switch (process.platform) {
+            case 'win32': {
+                throw new Error('Unsupported platform: ${process.platform}')
+            }
+            case 'darwin': {
+                throw new Error('Unsupported platform: ${process.platform}')
+            }
         }
-      )
-    })
 
-    core.summary.addHeading('Test results')
+        await core.group('Run bedrock server', async () => {
+            await exec.exec(
+                path.join(process.cwd(), BDS_PATH, 'bedrock_server'),
+                [],
+                {
+                    cwd: path.join(process.cwd(),)
+                }
+            )
+        })
 
-    const globber = await glob.create(
-      path.join(process.cwd(), BDS_PATH, 'ContentLog__*')
-    )
-    for await (const cl of globber.globGenerator()) {
-      const flbuf = await readFile(cl, 'utf8')
-      core.summary.addSeparator()
-      core.summary.addHeading(cl)
-      core.summary.addCodeBlock(flbuf)
+        core.summary.addHeading('Test results')
+
+        const globber = await glob.create(
+            path.join(process.cwd(), BDS_PATH, 'ContentLog__*')
+        )
+        for await (const cl of globber.globGenerator()) {
+            const flbuf = await readFile(cl, 'utf8')
+            core.summary.addSeparator()
+            core.summary.addHeading(cl)
+            core.summary.addCodeBlock(flbuf)
+        }
+
+        core.summary.write()
+    } catch (error) {
+        if (error instanceof Error) core.setFailed(error.message)
     }
-
-    core.summary.write()
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
-  }
 }
 
 const sleep = (ms: number) => new Promise(r => setTimeout(r, ms))
